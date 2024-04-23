261f2512b9634a515df55f3e33d2cf2d76c68949
TanmayChaurasia24 <tanmaychaurasia88@gmail.com>
2024-04-23 17:57:34 +0530

    b

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,27 @@
+// Import the mongoose library for MongoDB interactions
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js";
 
+// Import the DB_NAME constant from the constants.js file
+import { DB_NAME } from "../constants.js";
+
+// Define an asynchronous function named connectDB to establish a connection with the MongoDB database
 const connectDB = async () => {
     try {
+        // Attempt to establish a connection with the MongoDB database using the mongoose.connect() method
+        // ${process.env.mongodb_uri} is the MongoDB URI provided as an environment variable
+        // DB_NAME is the name of the MongoDB database
         const connectionInstance = await mongoose.connect(`${process.env.mongodb_uri}/${DB_NAME}`);
+
+        // If the connection is successful, log a success message to the console
         console.log(`\nMongoDB connected!! DB host: ${connectionInstance.connection.host}`);
     } catch (error) {
+        // If an error occurs during the connection attempt, log the error to the console
         console.log("Mongo connection error: ", error);
-        process.exit(1); // We can use throw error also 
+
+        // Terminate the Node.js process with an exit code of 1 (indicating an error)
+        process.exit(1); // We can use throw error also
     }
 };
 
+// Export the connectDB function as the default export of this module
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 // require('dotenv').config({path:'./env'}) not use this we have improved version for it 
 
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+// Import the dotenv library to load environment variables from a .env file
+import dotenv from "dotenv";
 
+// Import the connectDB function from the ./db/index.js file
+import connectDB from "./db/index.js";
+
+// Load environment variables from a .env file into process.env
 dotenv.config({
     path: './env'
-})
+});
 
-
-// this returns a promise
+// Call the connectDB function to establish a connection with the MongoDB database
+// This function returns a Promise
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, ()=> {
-        console.log(`server is running on port : ${process.env.PORT}`);
+    .then(() => {
+        // If the connection is successful, start the server and listen for incoming requests
+        // The server listens on the port specified in the environment variable PORT, or port 8000 if PORT is not defined
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port : ${process.env.PORT}`);
+        });
     })
-})
-.catch((error) => {
-    console.log("mango db connection failed!!", error);
-})
+    .catch((error) => {
+        // If an error occurs during the database connection attempt, log the error to the console
+        console.log("MongoDB connection failed!!", error);
+    });
 
 
 
