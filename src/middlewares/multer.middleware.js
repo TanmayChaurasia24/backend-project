ae16544602b96b2d338e83315222e61d74d14d69
TanmayChaurasia24 <tanmaychaurasia88@gmail.com>
2024-04-25 18:12:09 +0530

    adding flies,video etc to cloudinary with the help of multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+// Importing the multer library for handling file uploads
+import multer from "multer";
+
+// Configuring multer disk storage for storing uploaded files
+const storage = multer.diskStorage({
+    // Setting the destination directory where uploaded files will be stored
+    destination: function (req, file, cb) {
+        cb(null, './public/temp'); // Destination directory path
+    },
+    // Setting the filename for the uploaded file
+    filename: function (req, file, cb) {
+        // Using the original filename of the uploaded file
+        cb(null, file.originalname); // Original filename is used as the file's name
+    }
+});
+
+// Creating a multer upload middleware with the configured storage settings
+export const upload = multer({ storage: storage });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+// Importing the cloudinary v2 SDK and the fs module from Node.js
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuring cloudinary with the API credentials obtained from environment variables
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Defining an asynchronous function to upload a file to cloudinary
+const uploadoncloudinary = async (localfilepath) => {
+  try {
+    // Check if local file path exists
+    if (!localfilepath) {
+      return null; // If not, return null
+    } else {
+      // If file path exists, upload the file to cloudinary
+      const response = await cloudinary.uploader.upload(localfilepath, {
+        resource_type: "auto" // this will automatically detect the wether it is a file,video,pdf etc
+      });
+
+      // Log success message and return cloudinary response
+      console.log("File has uploaded successfully on cloudinary: ", response.url);
+      return response;
+    }
+  } catch (error) {
+    // If an error occurs during upload, delete the local file and return null
+    fs.unlinkSync(localfilepath); // delete the local file as error occurred while uploading the file
+    return null;
+  }
+}
+
+// Export the uploadoncloudinary function for use in other modules
+export { uploadoncloudinary };
